2471620c8c37d6b2308540df1d89c3e46193552d
beidemichael <beidemichael@gmail.com>
2023-11-11 16:30:35 +0300

    added assets

--- lib/models/video.dart
+++ lib/models/video.dart
@@ -14,15 +14,36 @@ List<VideoModel> videoList = [
       caption:
           'Lorem ipsum dolor sit amet consectetur. Eget aenean integer amet sapien arcu urna turpis amet elementum. A nec euismod in quam venenatis. Consectetur et nunc amet mattis dui imperdiet tempus. Et aliquet gravida posuere pretium donec diam nibh sed. Pharetra non vitae urna aliquet egestas. Phasellus at id adipiscing eget. Lorem volutpat dolor lorem pharetra nunc duis lorem integer. Magna in pellentesque pretium elementum suspendisse tincidunt fermentum praesent',
       date: '03 Nov 2022 @1:23pm',
-      video: 'https://assets.mixkit.co/videos/preview/mixkit-taking-photos-from-different-angles-of-a-model-34421-large.mp4'),
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-hands-holding-a-smart-watch-with-the-stopwatch-running-32808-large.mp4'),
   VideoModel(
       caption:
           'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       date: '27 Oct 2020 @5:23pm',
-      video: 'https://assets.mixkit.co/videos/preview/mixkit-taking-photos-from-different-angles-of-a-model-34421-large.mp4'),
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-man-runs-past-ground-level-shot-32809-large.mp4'),
   VideoModel(
       caption:
           'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
       date: '11 Jan 2021 @6:03pm',
-      video: 'https://assets.mixkit.co/videos/preview/mixkit-taking-photos-from-different-angles-of-a-model-34421-large.mp4'),
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-man-doing-tricks-with-roller-skates-in-a-parking-lot-34553-large.mp4'),
+  VideoModel(
+      caption:
+          'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      date: '11 Jan 2021 @6:03pm',
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-curving-a-road-while-cars-speeding-by-34564-large.mp4'),
+  VideoModel(
+      caption:
+          'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      date: '11 Jan 2021 @6:03pm',
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-two-avenues-with-many-cars-traveling-at-night-34562-large.mp4'),
+  VideoModel(
+      caption:
+          'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      date: '11 Jan 2021 @6:03pm',
+      video:
+          'https://assets.mixkit.co/videos/preview/mixkit-city-traffic-on-bridges-and-streets-34565-large.mp4'),
 ];
--- lib/screens/1.apps.dart
+++ lib/screens/1.apps.dart
@@ -15,20 +15,55 @@ class AppsPage extends StatefulWidget {
 }
 
 class _AppsPageState extends State<AppsPage> {
+  int currentPageIndex = 0;
+  void loadNextVideos() {
+    // Determine the range of indices for the next 3 videos
+    int startIndex = currentPageIndex + 1;
+    int endIndex = currentPageIndex + 4; // Load the next 3 videos
+
+    // Ensure endIndex does not exceed the length of the videoList
+    endIndex = endIndex.clamp(0, videoList.length - 1);
+
+    // Fetch and add the next videos to the list
+    for (int i = startIndex; i <= endIndex; i++) {
+      // Check if the video at index i is not already in the list
+      if (!videoList.contains(videoList[i])) {
+        // Fetch the video details and add it to the list
+        VideoModel newVideo = VideoModel(
+          caption: videoList[i].caption,
+          date: videoList[i].date,
+          video: videoList[i].video,
+        );
+        videoList.add(newVideo);
+      }
+    }
+
+    // Update the state to trigger a rebuild
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: PageView.builder(
-      itemCount: videoList.length,
-      controller: PageController(viewportFraction: 1),
-      itemBuilder: (BuildContext context, int itemIndex) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 4.0),
-          child: VideoView(
+      child: PageView.builder(
+        itemCount: videoList.length,
+        scrollDirection: Axis.vertical,
+        controller: PageController(viewportFraction: 1),
+        onPageChanged: (int index) {
+          // Update the current index when the page changes
+          setState(() {
+            currentPageIndex = index;
+          });
+
+          // Load the next 3 videos if available
+          loadNextVideos();
+        },
+        itemBuilder: (BuildContext context, int itemIndex) {
+          return VideoView(
             videoModel: videoList[itemIndex],
-          ),
-        );
-      },
-    ));
+          );
+        },
+      ),
+    );
   }
 }
